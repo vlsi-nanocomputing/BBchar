70e2e6aed9adeb5c24561d1f42d1171d55bf3c29
Flavio Lupoli <s287624@studenti.polito.it>
2023-01-09 15:44:19 +0100

    most recent version

--- characterization.m
+++ characterization.m
@@ -28,18 +28,20 @@ function characterization(charSettings,file,terminationSettings,terminationCircu
     N_clk_regions = driverPara.NclockRegions;   %number of clock regions (phases) of the layout
     max_Voltage = driverPara.maxVoltage;    %maximum voltage (abs value) the driver will assume in Volts
 
-    A = golden_outMol_finder(simulation_path, bus_flag, term_angle);
+    A = outMol_finder(simulation_path, bus_flag, term_angle);
 
     % saving the names of the drivers in a string array
     N_drivers = tableNMol.stack_driver.num;
     for dd = 1:N_drivers
-        tmp_driver_label = tableNMol.stack_driver.stack(dd).identifier_qll;
-        driver_labels(dd,:) = strcat('driver_',tmp_driver_label);
+        tmp_driver_label_qll = tableNMol.stack_driver.stack(dd).identifier_qll;
+        driver_labels_qll(dd,:) = strcat('driver_',tmp_driver_label_qll);
+        tmp_driver_label = tableNMol.stack_driver.stack(dd).identifier;
+        driver_labels{dd,:} = tmp_driver_label;
     end
 
     for a = 2:length(table.Properties.VariableDescriptions)        % scanning the names of the original table's columns
         heading = cell2mat(table.Properties.VariableDescriptions(1,a));
-        for b = 1:A.N_outputs
+        for b = 1:A.N_outputs*2
             if strcmp(heading,A.clock_labels(b,:)) == 1       %if the title corresponds to one of the clock names ...
                 clock_mat(:,b) = table{:,a};                   %the entire column of the table is copied in clock_mat
             elseif strcmp(heading,A.output_labels(b,:)) == 1   %same thing for the output names
@@ -47,10 +49,9 @@ function characterization(charSettings,file,terminationSettings,terminationCircu
             elseif strcmp(heading,A.original_clock_labels(b,:)) == 1
                 original_clock_mat(:,b) = table{:,a};
             end
-
         end
         for c = 1:N_drivers
-            if strcmp(heading,driver_labels(c,:)) == 1
+            if strcmp(heading,driver_labels_qll(c,:)) == 1
                 driver_mat(:,c) = table{:,a};
             end
         end
@@ -61,28 +62,49 @@ function characterization(charSettings,file,terminationSettings,terminationCircu
     %The next step is to check the time steps in which the selected clocks
     %are in HOLD, in order to save the equivalent outputs and drivers
 
-    saved_outputs = NaN(length(output_mat),1);            %preallocating output array
-    saved_drivers = NaN(length(driver_mat),N_drivers);    %preallocating driver array
-
+    % removing complementary drivers (i.e. Dr1_c, Dr2_c, Dr3_c)
     for j = 1:N_drivers
-        dr_table{j} = driver_labels(j,:);     %table containing the names of the drivers
+        tmp_str = char(driver_labels(j));
+        if tmp_str(end) ~= 'c'
+            dr_table{j} = driver_labels(j,:);     %table containing the (true) names of the drivers
+            comp_array{j} = j;    %this array tells which columns of the driver_mat to eliminate 
+        end
     end
-    dr_array = string(dr_table);    %array containing the names of the drivers (it's fixed)
+    dr_table = dr_table(~cellfun('isempty',dr_table));         %removing empty values 
+    comp_array = comp_array(~cellfun('isempty',comp_array));   %removing empty values   
+    dr_array = string(dr_table);
+    comp_array = cell2mat(comp_array);
+    for jj = length(comp_array):-1:1
+        driver_mat(:,comp_array(jj)) = [];   %removing all the columns that contain the data related to complementary drivers
+    end
 
-    
+    saved_outputs = NaN(length(output_mat),1);             %preallocating output array
+    saved_drivers = NaN(length(driver_mat),N_drivers/2);   %preallocating driver matrix
+
+    %the aim of the following for cycle is to rename the drivers in the
+    %dr_array. This is necessary since there are two columns for 'Dr1', two
+    %for 'Dr2' and so on. Each couple of identical drivers is named as, for instance, 'Dr1_a'
+    %and 'Dr1_b' in order to distinguish the two.
+    for y = 1:2:length(dr_array)-1
+       dr_array(y) = strcat(dr_array(y),'_a');
+       dr_array(y+1) = strcat(dr_array(y+1),'_b');
+    end
+
+    ABCD_names = reorder(A.out_coord).final_string;   %substituting the original labels with Vout_A, ecc.
+
     %%%%%%%%    generating a .csv file for each output    %%%%%%%%
-    for d = 1:A.N_outputs
-        headers_array = [A.output_labels(d,:) dr_array];   %temporary array of strings containing the headers of the .csv file
+    for d = 1:A.N_outputs*2
+        headers_array = [ABCD_names(d,:) dr_array];   %array of strings containing the headers of the d-th .csv file
         for e = 1:length(clock_mat)
-            if clock_mat(e,d) == 2 && original_clock_mat(e,d) == 2      %check if there is HOLD state
+            if clock_mat(e,d) == 2 && original_clock_mat(e,d) == 2      %check if output and termination are in HOLD state
                 saved_outputs(e,1) = output_mat(e,d);  %outputs that are going to be stored in the .csv file
                 saved_drivers(e,:) = driver_mat(e,:);  %drivers that are going to be stored in the .csv file
             end
         end
         final_mat = rmmissing(cat(2,saved_outputs,saved_drivers));  %concatenating outputs and drivers in a matrix and then removing NaN values from it
         T = array2table(final_mat,'VariableNames',headers_array);   %creating the table containing output and drivers
-        file_label = A.output_labels(d,:);
-        file_name = strcat(file_label,'.csv');
+        T = unique(T);   %eliminating equal rows
+        file_name = strcat(ABCD_names(d,:),'.csv');
         path = fullfile(dirPath,file_name);
         writetable(T,path);   %creation of the .csv file
     end
@@ -116,9 +138,9 @@ function characterization(charSettings,file,terminationSettings,terminationCircu
 
     %%%%%%%%%%    writing the .txt file with additional info    %%%%%%%%%%
     fileid = fopen(fullfile(dirPath,'info.txt'),"w");
-    fprintf(fileid,'Number of outputs: %d\n',A.N_outputs);
-    for w = 1:A.N_outputs
-        fprintf(fileid,'%s ',A.output_labels(w,:));
+    fprintf(fileid,'Number of outputs: %d\n',A.N_outputs*2);
+    for w = 1:A.N_outputs*2
+        fprintf(fileid,'%s ',ABCD_names(w,:));
     end
     fprintf(fileid,'\nLatency of the circuit is %d',latency);
     fprintf(fileid,'\nNumber of clock phases is %d',N_clk_regions);
