04aef3c96eea8a605cf94de59c96efab59d976d0
Flavio Lupoli <s287624@studenti.polito.it>
2023-01-15 18:01:38 +0100

    differently from reorder.m, now the outputs are not sorted (i.e. their order remains the same) but simply renamed as Vout_A, Vout_B and so on. Also, the output Vout that the function returns is not an array but a struct which contains all these renamed outputs of the circuit

---
+++ rename_outputs.m
@@ -0,0 +1,26 @@
+%% function to assign the names Vout_A, Vout_B, Vout_C, Vout_D
+
+function [ordered_data] = rename_outputs(out_coord)
+
+    min_z = min(out_coord(:,3));
+    max_z = max(out_coord(:,3));
+    min_y = min(out_coord(:,2));
+    max_y = max(out_coord(:,2));
+    for j = 1:length(out_coord)
+        if out_coord(j,3) == max_z
+            if out_coord(j,2) == min_y
+                out_labels(j,:) = 'Vout_A';
+            elseif out_coord(j,2) == max_y
+                out_labels(j,:) = 'Vout_C';
+            end
+        elseif out_coord(j,3) == min_z
+            if out_coord(j,2) == min_y
+                out_labels(j,:) = 'Vout_B';
+            elseif out_coord(j,2) == max_y
+                out_labels(j,:) = 'Vout_D';
+            end
+        end
+    end
+
+    ordered_data.final_string = out_labels;
+end
