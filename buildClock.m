10dfb9d6d12927d86da3896c32c427197a5f4e41
Flavio Lupoli <s287624@studenti.polito.it>
2023-01-31 10:13:56 +0100

    length of the clock signals is adapted to the length of the drivers in the 4-phase layout: filler does not need to be added

--- buildClock.m
+++ buildClock.m
@@ -1,8 +1,7 @@
 function [stack_phase] = buildClock(NclockRegions,NsweepSteps,phasesRepetition,pReset,pCycle,driverModes)
 
-filler = repmat(pReset,1,(NclockRegions - 1));
 Ncomb = size(driverModes);
-completeCycle = repmat([repmat(pCycle, 1, NsweepSteps + phasesRepetition - 1) filler ],1,Ncomb(1));
+completeCycle = repmat([repmat(pCycle, 1, NsweepSteps + phasesRepetition - 1)],1,Ncomb(1));
 stack_phase = zeros(NclockRegions,length(completeCycle));
 
 for ii = 1:NclockRegions
