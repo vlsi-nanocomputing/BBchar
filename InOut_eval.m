bdcc0d13a3d220e5777de6350192143cbace77e1
Flavio Lupoli <s287624@studenti.polito.it>
2023-03-20 15:04:33 +0100

    In this version of the function, the search for the output is performed for each molecule belonging to the output cells of the layout and not just for Vout_A and Vout_C

--- InOut_eval.m
+++ InOut_eval.m
@@ -9,31 +9,27 @@ function [Vout] = InOut_eval(sim_path,file,input)
     fileid = fopen(fullfile(dirPath,'info.txt'),"r");   %open the file that contains the names of the output files
     first_line = fgets(fileid);   %from the first line of the .txt file I read the names of the output files
     N_outputs = str2double(first_line(20));   %number of outputs --> number of files that are going to be read
-    Vout = NaN(1,N_outputs);   %preallocating the temporary Vout array (it will contain as many values as the number of outputs)
-    outputs = cell2mat(split(fgetl(fileid)));   %gets the names of the outputs in a string column array
+    outputs = split(fgetl(fileid));   %gets the names of the outputs in a string column array
 
     for j = 1:N_outputs
-        if strcmp(outputs(j,:),'Vout_A') || strcmp(outputs(j,:),'Vout_C')
-            file_name_tmp = strcat(outputs(j,:),'.csv');
-            out_mat = table2array(readtable(fullfile(dirPath,file_name_tmp)));
-            
-            %the drivers of the file are read to see if they correspond to the given inputs
-            mat_size = size(out_mat);
-            Ncol = mat_size(2);
-            vector = zeros(1,Ncol-1);
-            for c = 2:Ncol
-                [~,pos] = min(abs(out_mat(:,c)-input(c-1)));   % finding the value in the c-th column that is nearest to the input one
-                vector(c-1) = out_mat(pos,1);  %contains all the output that correspond to the inputs (the nearest ones)
-            end
-            nearest = sum(vector)/length(vector);    %evaluating an average value of the output values in vector
-            [~,pos_near] = min(abs(out_mat(:,1)-nearest));
-            Vout(j) = out_mat(pos_near,1);    %final output value is the closest one with respect to the average value
+        file_name_tmp = strcat(outputs{j,:},'.csv');
+        out_mat = table2array(readtable(fullfile(dirPath,file_name_tmp)));
+        
+        %the drivers of the file are read to see if they correspond to the given inputs
+        mat_size = size(out_mat);
+        Ncol = mat_size(2);
+        vector = zeros(1,Ncol-1);
+        for c = 2:Ncol
+            [~,pos] = min(abs(out_mat(:,c)-input(c-1)));   % finding the value in the c-th column that is nearest to the input one
+            vector(c-1) = out_mat(pos,1);  %contains all the output that correspond to the inputs (the nearest ones)
         end
+        nearest = sum(vector)/length(vector);    %evaluating an average value of the output values in vector
+        [~,pos_near] = min(abs(out_mat(:,1)-nearest));
 
+        % loading the output structure with the names of the outputs and the corresponding values
+        Vout.(outputs{j,:}) = out_mat(pos_near,1);  
     end
-
-    Vout = rmmissing(Vout);
-
 end
+
 
     
