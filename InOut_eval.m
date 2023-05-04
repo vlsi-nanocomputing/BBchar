739855e6e69dccf146caa31a7f6c351cd4d549e8
Flavio Lupoli <s287624@studenti.polito.it>
2023-05-04 21:45:20 +0200

    Ultimate version of the file. Differently from the previous version, it takes the rows of inputs with the highest number of values closest to the given one and returns the corresponding output.

--- InOut_eval.m
+++ InOut_eval.m
@@ -1,6 +1,5 @@
 %% InOut_eval function
 
-
 function [Vout] = InOut_eval(sim_path,file,input)
 
     starting_path = fullfile(sim_path,'charResults');   %the path from which the .csv files are taken
@@ -11,25 +10,40 @@ function [Vout] = InOut_eval(sim_path,file,input)
     N_outputs = str2double(first_line(20));   %number of outputs --> number of files that are going to be read
     outputs = split(fgetl(fileid));   %gets the names of the outputs in a string column array
 
-    for j = 1:N_outputs
-        file_name_tmp = strcat(outputs{j,:},'.csv');
+    for k = 1:N_outputs
+        file_name_tmp = strcat(outputs{k,:},'.csv');
         out_mat = table2array(readtable(fullfile(dirPath,file_name_tmp)));
         
         %the drivers of the file are read to see if they correspond to the given inputs
         mat_size = size(out_mat);
         Ncol = mat_size(2);
-        vector = zeros(1,Ncol-1);
+        diff_rows = mat_size(1);
+        diff_cols = mat_size(2) - 1;
+        diff_mat = zeros(diff_rows,diff_cols);
+        column_min = zeros(1,diff_cols);
+  
         for c = 2:Ncol
-            [~,pos] = min(abs(out_mat(:,c)-input(c-1)));   % finding the value in the c-th column that is nearest to the input one
-            vector(c-1) = out_mat(pos,1);  %contains all the output that correspond to the inputs (the nearest ones)
+            diff_mat(:,c-1) = abs(out_mat(:,c)-input(c-1));   % finding the value in the c-th column that is nearest to the input one
+            column_min(c-1) = min(diff_mat(:,c-1));   %minimum value of each column
         end
-        nearest = sum(vector)/length(vector);    %evaluating an average value of the output values in vector
-        [~,pos_near] = min(abs(out_mat(:,1)-nearest));
+
+        %arrow that contains the number of minima in a row (its dimension
+        %is the number of rows)
+        cnt_min = zeros(1,diff_rows);   
+
+        for i = 1:diff_rows
+            for j = 1:diff_cols
+                if diff_mat(i,j) == column_min(j)
+                    cnt_min(i) = cnt_min(i) + 1;
+                end
+            end
+        end
+
+        [~,out_pos] = max(cnt_min);
 
         % loading the output structure with the names of the outputs and the corresponding values
-        Vout.(outputs{j,:}) = out_mat(pos_near,1);  
+        Vout.(outputs{k,:}) = out_mat(out_pos,1);  
     end
 end
-
 
     
