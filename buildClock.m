556e2480f76fc7b6bf9f3bc5aa48af4ac601a408
Flavio Lupoli <s287624@studenti.polito.it>
2023-05-04 21:53:39 +0200

    Ultimate version of the function. It is suited for any number of combinations, since the clock cycle is repeated accordingly to it. The filler is added only at the very end.

--- buildClock.m
+++ buildClock.m
@@ -1,12 +1,13 @@
 function [stack_phase] = buildClock(NclockRegions,NsweepSteps,phasesRepetition,pReset,pCycle,driverModes)
 
-Ncomb = size(driverModes);
-completeCycle = repmat([repmat(pCycle, 1, NsweepSteps + phasesRepetition - 1)],1,Ncomb(1));
-stack_phase = zeros(NclockRegions,length(completeCycle));
-
-for ii = 1:NclockRegions
-    stack_phase(ii,:) = circshift(completeCycle,length(pReset)*(ii-1));
-end
-
+    size_modes = size(driverModes);
+    Ncomb = size_modes(1);
+    filler = repmat(pReset,1,(NclockRegions - 1));
+    completeCycle = [repmat(pCycle,1,NsweepSteps*Ncomb + phasesRepetition - 1) filler];
+    stack_phase = zeros(NclockRegions,length(completeCycle));
+    
+    for ii = 1:NclockRegions
+        stack_phase(ii,:) = circshift(completeCycle,length(pReset)*(ii-1));
+    end
 
 end
